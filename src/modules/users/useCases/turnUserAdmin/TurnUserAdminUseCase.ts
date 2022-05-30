6bcd82047b08233bd0639a0d0ba752b4ef613b6f
Luiza Saquette <lu.saquette@gmail.com>
2022-05-30 16:17:53 -0300

    feat: turn user admin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -6,10 +6,18 @@ interface IRequest {
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("Invalid user!");
+    }
+
+    const adminUser = this.usersRepository.turnAdmin(user);
+
+    return adminUser;
   }
 }
 
